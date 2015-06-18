a7c353f9421830f883e0f74d38f5a1450659c1f9
Manuel Dias Marques <mfdiasmarques@gmail.com>
2015-06-18 18:50:55 +0100

    Structure project committed.

---
+++ T3/WcfService1/WcfService1/IGamePlayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    [ServiceContract]
+    public interface IGame
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        
+    }
+    
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IGamePlayer : IGame
+    {
+        // TODO: Add your service operations here
+        [OperationContract]
+        string SetPlay(int n1, int n2);
+
+       
+    }
+
+    [ServiceContract]
+    public interface IGameManager : IGame
+    {
+        [OperationContract]
+        string StartGame(int n1, int n2);
+
+        [OperationContract]
+        string EndGame();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
